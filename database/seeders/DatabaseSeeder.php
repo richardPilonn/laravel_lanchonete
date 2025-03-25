baa6ea6030664d7b19513a7c8f67dc79ecbecee5
richardPilonn <richard.pilonzera@gmail.com>
2025-03-25 13:57:46 -0300

    seeder do funcionario e alterações

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -17,6 +17,7 @@ class DatabaseSeeder extends Seeder
         $this->call([
             clienteSeeder::class,
             produtoSeeder::class,
+            funcionarioSeeder::class,
         ]);
     }
 }
